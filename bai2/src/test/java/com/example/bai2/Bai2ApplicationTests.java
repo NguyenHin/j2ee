861e864f151658fa9b9a3c958d02d96908012e92
NguyenHin <nguyenhien8148@gmail.com>
2026-01-31 14:18:50 +0700

    add bai2

---
+++ bai2/src/test/java/com/example/bai2/Bai2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bai2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class bai2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
